4e4a7de51beb54de48ac11f3ef16c1b8ea940484
Anton Piskunov <anton@piskunov.email>
2020-12-28 09:43:38 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,9 +7,13 @@
 
 # If GITHUB_WORKSPACE is set, prepend it to $1 for _config_path.
 echo "INFO: GITHUB_WORKSPACE is '${GITHUB_WORKSPACE}'."
-_config_path="${GITHUB_WORKSPACE%/}/${1:-public.yaml}"
 
-_extra_options="${2}"
+_config_path="${GITHUB_WORKSPACE%/}/${1:-public.yaml}"
+_do_it="${2}"
+_force="${3}"
+_source="${4}"
+_target="${5}"
+_debug="${6}"
 
 # Change to config directory, so relative paths will work.
 cd "$(dirname "${_config_path}")" || echo "INFO: Cannot cd to $(dirname "${_config_path}")."
@@ -39,5 +43,26 @@ fi
 # Run octodns-sync.
 echo "INFO: _config_path: ${_config_path}"
 
-script "${GITHUB_WORKSPACE}/octodns-sync.log" -e -c \
-  "octodns-sync --config-file=\"${_config_path}\" ${_extra_options}"
+_cmd="octodns-sync --config-file=\"${_config_path}\""
+
+if [ "${_doit}" = "true" ]; then
+  _cmd="${_cmd} --doit"
+fi
+
+if [ "${_force}" = "true" ]; then
+  _cmd="${_cmd} --force"
+fi
+
+if [ ! -z "${_source}" ]; then
+  _cmd="${_cmd} --source=\"${_source}\""
+fi
+
+if [ ! -z "${_target}" ]; then
+  _cmd="${_cmd} --target=\"${_target}\""
+fi
+
+if [ "${_debug}" = "true" ]; then
+  _cmd="${_cmd} --debug"
+fi
+
+script "${GITHUB_WORKSPACE}/octodns-sync.log" -e -c "${_cmd}"
